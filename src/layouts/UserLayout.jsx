a111ac15d1cf7f3047b6a75154b16f46ebe2151c
nichiyoo <61191577+nichiyoo@users.noreply.github.com>
2024-08-13 07:56:18 +0700

    fix: minor css change for overflow and loading layout shifts

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,15 +1,17 @@
-import Footer from "@/components/user/Footer";
-import Navbar from "@/components/user/Navbar";
-import { Outlet } from "react-router-dom";
+import Footer from '@/components/user/Footer';
+import Navbar from '@/components/user/Navbar';
+import { Outlet } from 'react-router-dom';
 
 export function UserLayout() {
-    return (
-        <div>
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </div>
-    );
+	return (
+		<div>
+			<Navbar />
+			<div className='min-h-screen'>
+				<Outlet />
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default UserLayout;
